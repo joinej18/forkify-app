2e5fede7b5a85a373e77a17158ad8d80d95e81b7
unknown <joinej18.me@gmail.com>
2023-04-30 22:57:20 -0500

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '54af999f-d0a3-4868-9f15-74aa30a01c1b';
+export const MODAL_CLOSE_SEC = 2.5;
